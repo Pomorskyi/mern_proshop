9a4856d51bf821ae1e8bfef827496000c9f5aad1
Mykhailo Pomorskyi <mishapomorskyi@gmail.com>
2021-12-13 23:26:35 +0100

    Bootstrap comments

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -22,6 +22,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
+          /*Bootstrap sm md lg xl*/
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
